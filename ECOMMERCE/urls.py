586c0df9c472dd7cca1f8fc852a9ab4f6ad96e72
vinicius <viniciuseugeniovhe@gmail.com>
2023-03-06 13:18:53 -0300

    Initial

--- ECOMMERCE/urls.py
+++ ECOMMERCE/urls.py
@@ -1,7 +1,7 @@
 """ECOMMERCE URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
-    path('', include('product.urls')),
     path('admin/', admin.site.urls),
-    path('order/', include('order.urls')),
-    path('profile/', include('userprofile.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
